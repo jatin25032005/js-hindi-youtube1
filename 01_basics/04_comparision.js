2ab691e07c790b8d925cd9fc3c5c98a2cb91147b
jatin25032005 <aniketverma241303@gmail.com>
2026-03-19 23:20:05 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,21 +1,21 @@
-// console.log(2 > 1);
+// console.log(2 > 1);     
 // console.log(2 >= 1);
 // console.log(2 < 1);
 // console.log(2 == 1);
 // console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+// console.log("2" > 1);   true because convert it into int datatype
+// console.log("02" > 1);  true 
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null > 0);    f
+console.log(null == 0);   f   
+console.log(null >= 0);   t   treated null as 0
 
-console.log(undefined == 0);
-console.log(undefined > 0);
-console.log(undefined < 0);
+console.log(undefined == 0);   f
+console.log(undefined > 0);     f
+console.log(undefined < 0);    f
 
 // === 
 
-console.log("2" === 2);
+console.log("2" === 2);    return false because strictly check it number and datatype also
