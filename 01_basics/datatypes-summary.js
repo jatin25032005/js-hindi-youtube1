eb8c5832f046f220bcfe7004f62570b07b0ee07c
jatin25032005 <aniketverma241303@gmail.com>
2026-03-19 23:54:21 +0530

    Update datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -2,20 +2,20 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+const score = 100       javascript is dynamically typed so datatype is auto declare during runtime ;
+const scoreValue = 100.3   for this number is data type no concept of float and decimal
 
 const isLoggedIn = false
 const outsideTemp = null
 let userEmail;
 
-const id = Symbol('123')
+const id = Symbol('123')            either we give same value in symbol but symbol give diffrent and unique 
 const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
-
+// const bigNumber = 3456543576654356754n // using n we convert in bigint
+onsole.log(typeof anotherId); give undefined 
 
 
 // Reference (Non primitive)
