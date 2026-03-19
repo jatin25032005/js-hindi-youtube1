2993a40c9ddae30e20cbb4abd8c6a8bd007c4843
jatin25032005 <aniketverma241303@gmail.com>
2026-03-19 23:21:41 +0530

    Update 04_comparision.js

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -9,7 +9,7 @@
 // console.log("02" > 1);  true 
 
 console.log(null > 0);    f
-console.log(null == 0);   f   
+console.log(null == 0);   f   null is some question treated as 0 sometime uses as nan
 console.log(null >= 0);   t   treated null as 0
 
 console.log(undefined == 0);   f
