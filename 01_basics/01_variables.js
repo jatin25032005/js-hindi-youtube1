48de959277f5d1efb653f14d660d3a9327514cf3
jatin25032005 <aniketverma241303@gmail.com>
2026-03-19 20:40:08 +0530

    Update 01_variables.js

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,17 +1,17 @@
-const accountId = 144553
-let accountEmail = "hitesh@google.com"
-var accountPassword = "12345"
+const accountId = 144553  //which value is stored using const never be change
+let accountEmail = "hitesh@google.com" //its access in scope only 
+var accountPassword = "12345"   //  access everywhere
 accountCity = "Jaipur"
-let accountState;
+let accountState; // in which vakue is undefined
 
 // accountId = 2 // not allowed
-
+// we can cahange the value of let ,char , and without defing datatype value;
 
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
 
-console.log(accountId);
+console.log(accountId); // print value
 
 /*
 Prefer not to use var
@@ -19,4 +19,4 @@ because of issue in block scope and functional scope
 */
 
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) // use to print all the value in table formet;
