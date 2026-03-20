9b88d36245f4c9a92ec06ce685bc5e1153690c44
jatin25032005 <aniketverma241303@gmail.com>
2026-03-20 23:18:12 +0530

    Update 03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,4 +1,4 @@
-// singleton
+// singleton     jb constructor se declare kre tb singlton bnta hai 
 // Object.create
 
 // object literals
@@ -8,8 +8,9 @@ const mySym = Symbol("key1")
 
 const JsUser = {
     name: "Hitesh",
-    "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    "full name": "Hitesh Choudhary",          object literals in object we define key and value
+      mySym: "mykey1"       its works as string          
+    [mySym]: "mykey1",      its works as symbol
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -18,13 +19,13 @@ const JsUser = {
 }
 
 // console.log(JsUser.email)
-// console.log(JsUser["email"])
+// console.log(JsUser["email"])        also can access this types if email contain string than key ko string ki tarah hi do
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "hitesh@chatgpt.com"             value overwrite
+// Object.freeze(JsUser)   use all value freeze no changement perform now
+JsUser.email = "hitesh@microsoft.com"      value can,t change in object due to freeze            
 // console.log(JsUser);
 
 JsUser.greeting = function(){
@@ -34,5 +35,5 @@ JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+console.log(JsUser.greeting());      // Hello JS user
+console.log(JsUser.greetingTwo());    //Hello JS user ,hitesh
