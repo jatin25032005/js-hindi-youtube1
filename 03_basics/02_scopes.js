d3dec50d22a64941926b2cd7485caa683c936a5b
jatin25032005 <aniketverma241303@gmail.com>
2026-03-23 00:20:24 +0530

    Update 02_scopes.js

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,23 +1,24 @@
-//var c = 300
+//var c = 300            var  scope is global and also out of curly brases;
 let a = 300
 if (true) {
     let a = 10
     const b = 20
-    // console.log("INNER: ", a);
+    // console.log("INNER: ", a);          print 10;
     
 }
 
 
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
+// console.log(a);     //  300
+// console.log(b);     // undefined
+// console.log(c);     //undefined
 
+    global scope is different in chrom eand compliler
 
-function one(){
+function one(){                  this function can,t access inner function value 
     const username = "hitesh"
 
-    function two(){
+    function two(){                       inner function easily access outside value
         const website = "youtube"
         console.log(username);
     }
@@ -27,18 +28,18 @@ function one(){
 
 }
 
-// one()
+// one()             // one is called otherwise nothing done;
 
 if (true) {
     const username = "hitesh"
-    if (username === "hitesh") {
+    if (username === "hitesh") {                     
         const website = " youtube"
-        // console.log(username + website);
+        // console.log(username + website);               hitesh youtube ;
     }
-    // console.log(website);
+    // console.log(website);                             website not defind due to out of scope ;
 }
 
-// console.log(username);
+// console.log(username);                                 not defind
 
 
 // ++++++++++++++++++ interesting ++++++++++++++++++
@@ -46,13 +47,13 @@ if (true) {
 
 console.log(addone(5))
 
-function addone(num){
+function addone(num){                its valid we declare value above functiom if we can,t assign function to variable
     return num + 1
 }
 
 
 
 addTwo(5)
-const addTwo = function(num){
+const addTwo = function(num){       its not valid
     return num + 2
 }
