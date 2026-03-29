f5f02adc89b1e600051b8aed222262b1a0703cc4
jatin25032005 <aniketverma241303@gmail.com>
2026-03-29 20:20:06 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,19 @@
 // Immediately Invoked Function Expressions (IIFE)
-
+global scope ke pollution se bachne ke liye iifi ka use krte hai 
+    
 
 (function chai(){
-    // named IIFE
+    // named IIFE                     
     console.log(`DB CONNECTED`);
 })();
 
+
+
+do iifi ko execute krne ke liye first ke baad semicolon lga do;
+
+
 ( (name) => {
-    console.log(`DB CONNECTED TWO ${name}`);
+    console.log(`DB CONNECTED TWO ${name}`);               unnamed iifi 
+                                                            also give parameter as hitesh
 } )('hitesh')
 
