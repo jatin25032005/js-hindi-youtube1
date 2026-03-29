a32eb0ead9680c1138076cd6b8bca36c58b6664e
jatin25032005 <aniketverma241303@gmail.com>
2026-03-29 20:10:11 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,47 +3,63 @@ const user = {
     price: 999,
 
     welcomeMessage: function() {
-        console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        console.log(`${this.username} , welcome to website`);  this use acces to cureent context;
+        console.log(this);         give current context ;
     }
 
 }
 
-// user.welcomeMessage()
+// user.welcomeMessage()         hitesh welcome to wbsite;
 // user.username = "sam"
-// user.welcomeMessage()
+// user.welcomeMessage()         sam welcome to wbsite;
 
-// console.log(this);
-
+// console.log(this);         we in node inviornment so its refer to empty object ;
+                              now engine is standalone hai brouser ke andr global object window object hai
+                               
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username);          give undefined we cant use this inside function 
+// }
+
+// chai()
+
+
+// function chai(){
+//
+//   console.log(this);          so give various mircrotask ,fetch,time origin etc
 // }
 
 // chai()
 
 // const chai = function () {
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username);    give undefined;
+// }
+
+// const chai =  ()=> {
+//     let username = "hitesh"
+//     console.log(this.username);      give undefined;
 // }
 
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this);                   give empty parethesis ;
 }
 
 
 // chai()
 
-// const addTwo = (num1, num2) => {
+// const addTwo = (num1, num2) => {                 if you wraaped in curly vrases you need to write return 
+                                                       if in parethesis no need
 //     return num1 + num2
 // }
+console.log(addTwo(3, 4))               7
 
-// const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) =>  num1 + num2        implicit keyword return likhne ki jrurat nhi
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
-
-const addTwo = (num1, num2) => ({username: "hitesh"})
+// const addTwo = (num1, num2) => ( num1 + num2 )      if you wraaped in curly vrases you need to write return 
+                                                       if in parethesis no need 
+const addTwo = (num1, num2) => ({username: "hitesh"})         object ko return ke liyr parenthesis me wrapped krna pdenga
 
 
 console.log(addTwo(3, 4))
