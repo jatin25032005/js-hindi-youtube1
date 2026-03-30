a9593ba8fba3dd398a332163dc1025a6d250111e
jatin25032005 <aniketverma241303@gmail.com>
2026-03-30 23:55:58 +0530

    Update seven.js

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,8 +1,8 @@
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNumers.map( (num) => { return num + 10})
+// const newNums = myNumers.map( (num) => { return num + 10})     automatically return value
 
-const newNums = myNumers
+const newNums = myNumers                                            chaining esme ek map ka answer dusre map ko jata hai last final answer dispalay hota hai
                 .map((num) => num * 10 )
                 .map( (num) => num + 1)
                 .filter( (num) => num >= 40)
